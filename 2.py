d8f5e69736bd00ff12f4d07033caaeb3ca5ca0e7
KosenokIvan <kosenoki213@gmail.com>
2024-02-27 13:53:33 +0300

    Added bubble sort

--- 2.py
+++ 2.py
@@ -1,6 +1,7 @@
 import random
 import time
 import copy
+from typing import Callable, List
 
 user_m = int(input())
 user_n = int(input())
@@ -12,15 +13,43 @@ matrix = [[random.randint(user_min_limit, user_max_limit) for n in range(user_n)
 print(*matrix, sep="\n")
 print("=============")
 
-# Сортировка выбором
-matrix_copy = copy.deepcopy(matrix)
-start_time = time.time()
-for row in matrix_copy:
-    for i in range(user_n - 1):
-        min_index = i
-        for i1 in range(i + 1, user_n):
-            if row[i1] < row[min_index]:
-                min_index = i1
-        row[i], row[min_index] = row[min_index], row[i]
-print(*matrix_copy, sep="\n")
-print("--- {0} ms ---".format(round((time.time() - start_time)*1000)))
+
+def measure_time(sort_func: Callable[[List[List[int]]], None], matrix_: List[List[int]]):
+    matrix_copy = copy.deepcopy(matrix_)
+    start_time = time.time()
+    sort_func(matrix_copy)
+    print("--- {0} ms ---".format(round((time.time() - start_time) * 1000)))
+
+
+def choice_sort(matrix_: List[List[int]]):
+    for row in matrix_:
+        for i in range(len(row) - 1):
+            min_index = i
+            for i1 in range(i + 1, len(row)):
+                if row[i1] < row[min_index]:
+                    min_index = i1
+            row[i], row[min_index] = row[min_index], row[i]
+
+
+def insert_sort(matrix_: List[List[int]]):
+    for row in matrix_:
+        for i in range(1, len(row)):
+            insert_index = i
+            for i1 in range(i):
+                if row[i] <= row[i1]:
+                    insert_index = i1
+                    break
+            for i2 in range(i, insert_index, -1):
+                row[i2], row[i2 - 1] = row[i2 - 1], row[i2]
+
+
+def bubble_sort(matrix_: List[List[int]]):
+    for row in matrix_:
+        for i in range(len(row) - 1):
+            for i1 in range(len(row) - i - 1):
+                if row[i1] > row[i1 + 1]:
+                    row[i1], row[i1 + 1] = row[i1 + 1], row[i1]
+
+
+for sort_func in [choice_sort, insert_sort, bubble_sort]:
+    measure_time(sort_func, matrix)
